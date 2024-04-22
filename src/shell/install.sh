bd0c14f1fc333ea55eef40eee6276a1864667a02
Adam Sulucz <adam.sulucz@gmail.com>
2024-04-22 18:22:11 -0400

    Install updates

--- src/shell/install.sh
+++ src/shell/install.sh
@@ -49,6 +49,8 @@ ${bold}Options:${normal}
           [default: /opt/logship] Set install directory
       ${cyan}--data-root${normal}
           [default: /logship] Set the logship data directory
+      ${cyan}--preview${normal}
+          Download pre-release versions.
 
       ${cyan}--no-color${normal}
           Disable colorful output
@@ -75,17 +77,16 @@ err() {
     exit 1
 }
 
-# shellcheck disable=SC2059
 verbose_ship() {
     # Was this a waste of time? Maybe
     if [ "$opt_verbose" = 'true' ]; then
-        printf "         ${bold}${white}*${normal}    _______                                                     \n"
-        printf "           ${bold}${white}*${normal}_ |   ==|                                            _        \n"
-        printf "           ||_|     |${blue}[||][||]${green}[||]${red}[||]${normal}[||]${black}[||][||]${green}[||]${cyan}[||]${normal}[||]${cyan}[||]${normal}|        \n"
-        printf "          _||_|     |${cyan}[||]${magenta}[||]${green}[||][||]${black}[||]${red}[||]${normal}[||][||]${yellow}[||]${normal}[||]${magenta}[||]${normal}|___     \n"
+        printf "         %s*%s    _______                                                     \n" "$bold$white" "$normal" 
+        printf "           %s*%s  |   %s==%s|                                            %s_%s        \n" "$bold$white" "$normal" "$bold" "$normal" "$bold" "$normal"
+        printf "           ||_|     |%s[||]%s[||]%s[||]%s[||]%s[||]%s[||]%s[||]%s[||]%s[||]%s[||]%s[||]%s|%s        \n" "$blue" "$green" "$blue" "$red" "$white" "$green" "$white" "$red" "$white" "$blue" "$magenta" "$normal$bold" "$normal"
+        printf "          _||_|     |%s[||]%s[||]%s[||]%s[||]%s[||]%s[||]%s[||]%s[||]%s[||]%s[||]%s[||]%s|%s___     \n" "$red" "$white" "$blue" "$magenta" "$white" "$green" "$green" "$white" "$white" "$red" "$blue" "$normal$bold" "$normal"
         printf "         |......................................................o.../     \n"
         printf "         \........................................................./      \n"
-        printf "${bold}${blue}_,_,~_,~')_,~')_,~')_,~')_,~')_,~')_~')_,~')_~')_,~')_,~')_,~')_,~')_,~')${normal}\n\n"
+        printf "%s_,_,~_,~')_,~')_,~')_,~')_,~')_,~')_~')_,~')_~')_,~')_,~')_,~')_,~')_,~')%s\n\n" "$bold$blue" "$normal"
     fi
 }
 
@@ -93,11 +94,12 @@ invalid_arg () { printf >&2 "${bold}${red}%s:${normal}  %s: '${yellow}%s${normal
 check () { { [ "$1" != "$EOL" ] && [ "$1" != '--' ]; } || exit2 "missing argument" "$2"; }  # avoid infinite loop
 
 check_port() {
-    if [ $(echo "$1" | grep -q "^[0-9]+$") ] && [ "$1" -ge 0 ] && [ "$1" -le 65535 ]; then
-        return 0
-    else
-        err "Argument value \"$1\" is not a valid port."
-    fi
+  # shellcheck disable=SC2046,SC2143
+  if [ $(echo "$1" | grep -q "^[0-9]+$") ] && [ "$1" -ge 0 ] && [ "$1" -le 65535 ]; then
+      return 0
+  else
+      err "Argument value \"$1\" is not a valid port."
+  fi
 }
 
 opt_hostname='localhost'
@@ -106,8 +108,8 @@ opt_path='/opt/logship'
 opt_database_port='5000'
 opt_database_password="$(head /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 10)"
 opt_frontend_port='8000'
+opt_tag="latest-pre"
 
-tag="latest-pre"
 database_config_updated='false'
 database_password_updated='false'
 
@@ -115,7 +117,7 @@ database_password_updated='false'
 set -- "$@" "${EOL:=$(printf '\1\3\3\7')}"  # end-of-list marker
 while [ "$1" != "$EOL" ]; do
   opt="$1"; shift
-  # shellcheck disable=SC2295
+  # shellcheck disable=SC2295,SC2034
   case "$opt" in
     # Services
     -a | --agent     ) opt_agent=true ;;
@@ -141,6 +143,9 @@ while [ "$1" != "$EOL" ]; do
             check_port "$1"
             opt_frontend_port="$1";
             shift ;;
+         --preview        )
+            opt_tag='latest-pre'
+            ;;
          --no-install) opt_noinstall=true ;;
 
     # Installer Script Options
@@ -538,14 +543,14 @@ install_database() {
 
     verbose "Using temporary directory $tempdir"
     info "Downloading $service_name..."
-    verbose "Download URL: https://ar.logship.io/release/${service_name}/${architecture}/${tag}"
-    ensure wget "https://ar.logship.io/release/${service_name}/${architecture}/${tag}" -P "$tempdir" -q --show-progress
+    verbose "Download URL: https://ar.logship.io/release/${service_name}/${architecture}/${opt_tag}"
+    ensure wget "https://ar.logship.io/release/${service_name}/${architecture}/${opt_tag}" -P "$tempdir" -q --show-progress
     verbose "Downloaded $service_name."
     run_or_sudo mkdir -p "$opt_data_root"
     run_or_sudo mkdir -p "$database_path"
 
     verbose "Extracting files..."
-    ensure unzip -qq "$tempdir/$tag" -d "$tempdir/extract"
+    ensure unzip -qq "$tempdir/$opt_tag" -d "$tempdir/extract"
     rm "$tempdir/extract/appsettings.json" 2>/dev/null
     rm "$tempdir/extract/appsettings.Development.json" 2>/dev/null
 
@@ -625,142 +630,142 @@ EOF
 }
 
 install_frontend() {
-    frontend_path="$opt_path/frontend"
-    service_name="logship-frontend"
-    architecture="linux-x64"
-    config="appsettings.json"
-    exe="fe-react"
-    file="${service_name}_${architecture}.zip"
-    tempdir="$(mktemp -d -t "installer-$service_name-XXXXXXXXXXXXXXXX")"
+  frontend_path="$opt_path/frontend"
+  service_name="logship-frontend"
+  architecture="linux-x64"
+  config="appsettings.json"
+  exe="fe-react"
+  file="${service_name}_${architecture}.zip"
+  tempdir="$(mktemp -d -t "installer-$service_name-XXXXXXXXXXXXXXXX")"
 
-    run_or_sudo mkdir -p "$frontend_path"
-    verbose "Using temporary directory $tempdir"
-    info "Downloading $service_name..."
-    verbose "Download URL: https://ar.logship.io/release/${service_name}/${architecture}/${tag}"
-    ensure wget "https://ar.logship.io/release/${service_name}/${architecture}/${tag}" -P "$tempdir" -q --show-progress
-    verbose "Downloaded $service_name."
+  run_or_sudo mkdir -p "$frontend_path"
+  verbose "Using temporary directory $tempdir"
+  info "Downloading $service_name..."
+  verbose "Download URL: https://ar.logship.io/release/${service_name}/${architecture}/${opt_tag}"
+  ensure wget "https://ar.logship.io/release/${service_name}/${architecture}/${opt_tag}" -P "$tempdir" -q --show-progress
+  verbose "Downloaded $service_name."
 
-    verbose "Extracting files..."
-    ensure unzip -qq "$tempdir/$tag" -d "$tempdir/extract"
-    rm "$tempdir/extract/appsettings.json" 2>/dev/null
-    rm "$tempdir/extract/appsettings.Development.json" 2>/dev/null
+  verbose "Extracting files..."
+  ensure unzip -qq "$tempdir/$opt_tag" -d "$tempdir/extract"
+  rm "$tempdir/extract/appsettings.json" 2>/dev/null
+  rm "$tempdir/extract/appsettings.Development.json" 2>/dev/null
 
-    run_or_sudo cp -rf "$tempdir/extract/." "$frontend_path"
-    run_or_sudo chmod +x "$frontend_path/$exe"
+  run_or_sudo cp -rf "$tempdir/extract/." "$frontend_path"
+  run_or_sudo chmod +x "$frontend_path/$exe"
 
-    verbose "Cleaning up $tempdir"
-    rm -rf "$tempdir"
+  verbose "Cleaning up $tempdir"
+  rm -rf "$tempdir"
 
-    if [ ! -e "$frontend_path/$config" ] && [ -z "$opt_overwrite" ]; then
-        write_frontend_config "$frontend_path/$config"
-    else
-        if [ "$opt_overwrite" = 'true' ]; then
-            info "--overwrite: Overwriting existing frontend configuration."
-            write_frontend_config "$frontend_path/$config"
-        else
-            verbose "Frontend configuration \"$frontend_path/$config\" already exists. Use --overwrite to overwrite existing configuration."
-        fi
-    fi
+  if [ ! -e "$frontend_path/$config" ] && [ -z "$opt_overwrite" ]; then
+      write_frontend_config "$frontend_path/$config"
+  else
+      if [ "$opt_overwrite" = 'true' ]; then
+          info "--overwrite: Overwriting existing frontend configuration."
+          write_frontend_config "$frontend_path/$config"
+      else
+          verbose "Frontend configuration \"$frontend_path/$config\" already exists. Use --overwrite to overwrite existing configuration."
+      fi
+  fi
 
-    if [ -z "$opt_noinstall" ]; then
-        if systemctl list-unit-files | grep -q "^$service_name.service"; then
-            verbose "The $service_name service already exists"
-            # Check if the service is running
-            if systemctl is-active --quiet "$service_name"; then
-                # Stop the service
-                sudo systemctl stop "$service_name"
-                verbose "Stopped the $service_name service."
-            else
-                verbose "The $service_name service is not running."
-            fi
-        else
-            verbose "The $service_name service does not exist."
-            systemd_install "$service_name" "$opt_path/frontend" "$exe"
-        fi
-        
-        run_or_sudo systemctl enable "$service_name.service"
-        run_or_sudo systemctl restart "$service_name.service"
-    else
-        verbose "Skipping installation of $service_name"
-    fi
+  if [ -z "$opt_noinstall" ]; then
+      if systemctl list-unit-files | grep -q "^$service_name.service"; then
+          verbose "The $service_name service already exists"
+          # Check if the service is running
+          if systemctl is-active --quiet "$service_name"; then
+              # Stop the service
+              sudo systemctl stop "$service_name"
+              verbose "Stopped the $service_name service."
+          else
+              verbose "The $service_name service is not running."
+          fi
+      else
+          verbose "The $service_name service does not exist."
+          systemd_install "$service_name" "$opt_path/frontend" "$exe"
+      fi
+      
+      run_or_sudo systemctl enable "$service_name.service"
+      run_or_sudo systemctl restart "$service_name.service"
+  else
+      verbose "Skipping installation of $service_name"
+  fi
 
-    info "Configuration of $service_name complete."
+  info "Configuration of $service_name complete."
 }
 
 write_uninstall() {
-    verbose "Writing uninstall script to $opt_path/uninstall.sh"
-    if [ -e "$opt_path/uninstall.sh" ]; then
-        run_or_sudo rm -f "$opt_path/uninstall.sh"
-    fi
-    run_or_sudo tee -a "$opt_path/uninstall.sh" >/dev/null << EOF
+  verbose "Writing uninstall script to $opt_path/uninstall.sh"
+  if [ -e "$opt_path/uninstall.sh" ]; then
+      run_or_sudo rm -f "$opt_path/uninstall.sh"
+  fi
+  run_or_sudo tee -a "$opt_path/uninstall.sh" >/dev/null << EOF
 #!/bin/sh
 read -p "Uninstall logship? (y/n): This will delete everything under \"$opt_path\"" choice
 case "\$choice" in
-  [Yy]|[Yy][Ee][Ss])
-    systemctl disable logship-agent.service
-    systemctl disable logship-database.service
-    systemctl disable logship-frontend.service
-    systemctl stop logship-agent.service
-    systemctl stop logship-database.service
-    systemctl stop logship-frontend.service
-    rm -rf "$opt_path"
-    echo "Done"
-    exit 0
-    ;;
-  [Nn]|[Nn][Oo])
-    echo "Exiting"
-    exit 0
-    ;;
-  *)
-    echo "Invalid choice, please enter 'y' or 'n'."
-    exit 1
-    ;;
+[Yy]|[Yy][Ee][Ss])
+  systemctl disable logship-agent.service
+  systemctl disable logship-database.service
+  systemctl disable logship-frontend.service
+  systemctl stop logship-agent.service
+  systemctl stop logship-database.service
+  systemctl stop logship-frontend.service
+  rm -rf "$opt_path"
+  echo "Done"
+  exit 0
+  ;;
+[Nn]|[Nn][Oo])
+  echo "Exiting"
+  exit 0
+  ;;
+*)
+  echo "Invalid choice, please enter 'y' or 'n'."
+  exit 1
+  ;;
 esac
 EOF
-    run_or_sudo chmod +x "$opt_path/uninstall.sh"
+  run_or_sudo chmod +x "$opt_path/uninstall.sh"
 }
 
 main() {
-    need_cmds chmod echo find head mkdir mktemp rm rmdir sudo tee tr unzip wget
-    verbose_ship
-    verbose "Root installation path: $opt_path"
+  need_cmds chmod echo find head mkdir mktemp rm rmdir sudo tee tr unzip wget
+  verbose_ship
+  verbose "Root installation path: $opt_path"
 
-    if [ "$opt_agent" = 'true' ]; then
-        install_agent
-    fi
+  if [ "$opt_agent" = 'true' ]; then
+      install_agent
+  fi
 
-    if [ "$opt_database" = 'true' ]; then
-        install_database
-    fi
+  if [ "$opt_database" = 'true' ]; then
+      install_database
+  fi
 
-    if [ "$opt_frontend" = 'true' ]; then
-        install_frontend
-    fi
+  if [ "$opt_frontend" = 'true' ]; then
+      install_frontend
+  fi
 
-    # if [ "$opt_cli" = 'true' ]; then
-    #     install_logsh
-    # fi
+  # if [ "$opt_cli" = 'true' ]; then
+  #     install_logsh
+  # fi
 
-    write_uninstall
+  write_uninstall
 
-    info "Installation complete."
-    info "Uninstall with \"${cyan}sudo $opt_path/uninstall.sh${normal}\"."
-    if [ "$opt_database" = 'true' ] && [ "$database_config_updated" = 'true' ]; then
-        info "Your database is accessible at http://$opt_hostname:$opt_database_port."
-        if [ "$database_password_updated" = 'true' ]; then
-            info "    Username: admin"
-            info "    Password: $opt_database_password"
-            info "You should probably change this password. You can also find it in \"$opt_path/database/appsettings.json\"."
-        fi
-    fi
-    if [ "$opt_frontend" = 'true' ]; then
-        if systemctl is-active --quiet "logship-frontend"; then
-            info "Your frontend is accessible at http://$opt_hostname:$opt_frontend_port"
-        fi
-    fi
-    if [ "$opt_hostname" != 'localhost' ]; then
-        info "If you'd like to make your instance externally accessible, don't forget to update firewall rules!"
-    fi
+  info "Installation complete."
+  info "Uninstall with \"${cyan}sudo $opt_path/uninstall.sh${normal}\"."
+  if [ "$opt_database" = 'true' ] && [ "$database_config_updated" = 'true' ]; then
+      info "Your database is accessible at http://$opt_hostname:$opt_database_port."
+      if [ "$database_password_updated" = 'true' ]; then
+          info "    Username: admin"
+          info "    Password: $opt_database_password"
+          info "You should probably change this password. You can also find it in \"$opt_path/database/appsettings.json\"."
+      fi
+  fi
+  if [ "$opt_frontend" = 'true' ]; then
+      if systemctl is-active --quiet "logship-frontend"; then
+          info "Your frontend is accessible at http://$opt_hostname:$opt_frontend_port"
+      fi
+  fi
+  if [ "$opt_hostname" != 'localhost' ]; then
+      info "If you'd like to make your instance externally accessible, don't forget to update firewall rules!"
+  fi
 }
 
 main "$@" || exit 1
